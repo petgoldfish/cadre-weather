93f635b851dca7d01cfed9bd0fa8aa6fd4a12844
petgoldfish <petgoldfish@protonmail.com>
2021-04-25 18:25:36 -0400

    Style searchbox

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -1,7 +1,16 @@
 import { ReactElement } from "react";
+import "./Location.css";
 
 function Location(): ReactElement {
-  return <div className="card">Search</div>;
+  return (
+    <form className="flex-column">
+      <input
+        className="card search"
+        type="search"
+        placeholder="Search location..."
+      />
+    </form>
+  );
 }
 
 export default Location;
