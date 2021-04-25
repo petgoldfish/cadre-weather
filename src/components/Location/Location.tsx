88f1ea7200ed41f438edefd7c80feb5ca41623e5
petgoldfish <petgoldfish@protonmail.com>
2021-04-25 18:45:15 -0400

    Set up google places searchbox

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -1,15 +1,44 @@
-import { ReactElement } from "react";
+import { useState, ReactElement, useEffect } from "react";
+import GooglePlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-google-places-autocomplete";
 import "./Location.css";
 
+interface Loc {
+  label: string;
+}
+
 function Location(): ReactElement {
+  const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY!;
+
+  const [location, setLocation] = useState<Loc>();
+
+  useEffect(() => {
+    if (location) {
+      geocodeByAddress(location.label)
+        .then((results) => getLatLng(results[0]))
+        .then(({ lat, lng }) => console.log(lat, lng));
+    }
+  }, [location]);
+
   return (
-    <form className="flex-column">
-      <input
-        className="card search"
-        type="search"
-        placeholder="Search location..."
-      />
-    </form>
+    <GooglePlacesAutocomplete
+      apiKey={API_KEY}
+      debounce={300}
+      selectProps={{
+        value: location,
+        onChange: setLocation,
+        placeholder: "Enter Location...",
+        styles: {
+          container: (provided: any) => ({
+            ...provided,
+            marginBottom: 16,
+          }),
+        },
+      }}
+      minLengthAutocomplete={3}
+    />
   );
 }
 
